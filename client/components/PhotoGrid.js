66733bdbcac17b6ecc5d441f3850de52dd80f7f3
Henry Doan <henrydoan96@gmail.com>
2017-01-08 20:54:06 -0700

    created the single and photo grid component with app layout

---
+++ client/components/PhotoGrid.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Photogrid = React.createClass({
+	render() {
+		return (
+			<div className="photo-grid">
+			I'm a photoGrid
+			</div>
+		)
+	}
+});
+
+export default Photogrid;
