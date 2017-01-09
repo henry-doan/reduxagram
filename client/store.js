fee994dc69be102d64f50829fc5391f8f240821b
Henry Doan <henrydoan96@gmail.com>
2017-01-08 21:38:32 -0700

    created redux store

---
+++ client/store.js
@@ -0,0 +1,16 @@
+import { createStore, compse } from 'redux';
+import { syncHistoryWithStore } from 'react-router-redux';
+import { browserHistory } from 'react-router';
+import rootReducer from './reducers/index';
+import comments from './data/comments';
+import posts from './data/posts';
+
+const defaultState = {
+	posts,
+	comments
+};
+
+const store = createStore(rootReducer, defaultState);
+const history = syncHistoryWithStore(browerHistory, store);
+
+export default store;
